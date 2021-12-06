6921532d5d825ea36c099a603c9fea676f640d5a
Brian Wang <bwbrianwang1018@gmail.com>
2021-12-05 19:53:18 -0800

    Added handtrack component to app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,28 +15,30 @@ import { CarouselComponent } from './components/carousel/carousel.component';
 import { SearchComponent } from './components/search/search.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ThermometerComponent } from './components/thermometer/thermometer.component';
+import { HandtrackerComponent } from './components/handtracker/handtracker.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    CarouselCardComponent,
-    ArtistPageComponent,
-    AlbumPageComponent,
-    TrackPageComponent,
-    TrackListComponent,
-    CarouselComponent,
-    SearchComponent,
-    HomePageComponent,
-    ThermometerComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        CarouselCardComponent,
+        ArtistPageComponent,
+        AlbumPageComponent,
+        TrackPageComponent,
+        TrackListComponent,
+        CarouselComponent,
+        SearchComponent,
+        HomePageComponent,
+        ThermometerComponent,
+        HandtrackerComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
