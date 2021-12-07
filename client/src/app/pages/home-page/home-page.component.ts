1ce2d4b01fc9e4a7cb236ffbae1bf2cc2a321342
Brian Wang <60533686+bwangsta@users.noreply.github.com>
2021-12-06 16:35:38 -0800

    Added swiping gestures

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -3,26 +3,26 @@ import { PredictionEvent } from "../../prediction-event";
 import { SpotifyService } from "src/app/services/spotify.service";
 
 @Component({
-  selector: "app-home-page",
-  templateUrl: "./home-page.component.html",
-  styleUrls: ["./home-page.component.css"],
+    selector: "app-home-page",
+    templateUrl: "./home-page.component.html",
+    styleUrls: ["./home-page.component.css"],
 })
 export class HomePageComponent implements OnInit {
-  gesture: String = "";
-  constructor(private spotifyService: SpotifyService) {}
+    gesture: String = "";
+    constructor(private spotifyService: SpotifyService) { }
 
-  ngOnInit(): void {}
+    ngOnInit(): void { }
 
-  prediction(event: PredictionEvent) {
-    this.gesture = event.getPrediction();
-    if (this.gesture === "Two Closed Hands") {
-      window.location.href = this.spotifyService.link;
-    } else if (this.gesture === "Open Hand") {
-      window.location.href = "http://localhost:8888/login";
-    } else if (this.gesture === "Swiping Right") {
-      this.spotifyService.back();
-    } else if (this.gesture === "Swiping Left") {
-      this.spotifyService.forward();
+    prediction(event: PredictionEvent) {
+        this.gesture = event.getPrediction();
+        if (this.gesture === "Two Closed Hands") {
+            window.location.href = this.spotifyService.link;
+        } else if (this.gesture === "Closed Hand") {
+            window.location.href = "http://localhost:8888/login";
+        } else if (this.gesture === "Swiping Right") {
+            this.spotifyService.back();
+        } else if (this.gesture === "Swiping Left") {
+            this.spotifyService.forward();
+        }
     }
-  }
 }
