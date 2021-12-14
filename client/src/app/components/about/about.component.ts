b5f62233ef0b92473cd5c30d97e3f9e4f2c8d4bb
Brian Wang <bwbrianwang1018@gmail.com>
2021-12-13 17:13:06 -0800

    Added more styling

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -2,29 +2,29 @@ import { Component, Injectable, OnInit } from "@angular/core";
 import { SpotifyService } from "src/app/services/spotify.service";
 
 @Component({
-  selector: "app-about",
-  templateUrl: "./about.component.html",
-  styleUrls: ["./about.component.css"],
+    selector: "app-about",
+    templateUrl: "./about.component.html",
+    styleUrls: ["./about.component.css"],
 })
 @Injectable()
 export class AboutComponent implements OnInit {
-  name: string = null;
-  profile_pic: string = "../../../assets/unknown.jpg";
-  profile_link: string = null;
+    name: string = null;
+    profile_pic: string = "../../../assets/unknown.jpg";
+    profile_link: string = null;
 
-  //TODO: inject the Spotify service
-  constructor(private spotifyService: SpotifyService) {}
+    //TODO: inject the Spotify service
+    constructor(private spotifyService: SpotifyService) { }
 
-  ngOnInit() {}
+    ngOnInit() { }
 
-  /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
-    In that function, update the name, profile_pic, and profile_link fields */
-  getAboutMe() {
-    this.spotifyService.aboutMe().then((data) => {
-      this.name = data.name;
-      this.profile_pic = data.imageURL;
-      this.profile_link = data.spotifyProfile;
-      this.spotifyService.link = this.profile_link;
-    });
-  }
+    /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
+      In that function, update the name, profile_pic, and profile_link fields */
+    getAboutMe() {
+        this.spotifyService.aboutMe().then((data) => {
+            this.name = data.name;
+            this.profile_pic = data.imageURL;
+            this.profile_link = data.spotifyProfile;
+            this.spotifyService.link = this.profile_link;
+        });
+    }
 }
--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { PredictionEvent } from "../../prediction-event";
-import { SpotifyService } from "src/app/services/spotify.service";
 
 @Component({
     selector: "app-home-page",
@@ -8,21 +6,7 @@ import { SpotifyService } from "src/app/services/spotify.service";
     styleUrls: ["./home-page.component.css"],
 })
 export class HomePageComponent implements OnInit {
-    gesture: String = "";
-    constructor(private spotifyService: SpotifyService) { }
+    constructor() { }
 
     ngOnInit(): void { }
-
-    // prediction(event: PredictionEvent) {
-    //     this.gesture = event.getPrediction();
-    //     if (this.gesture === "Two Closed Hands") {
-    //         window.location.href = this.spotifyService.link;
-    //     } else if (this.gesture === "Closed Hand") {
-    //         window.location.href = "http://localhost:8888/login";
-    //     } else if (this.gesture === "Swiping Right") {
-    //         this.spotifyService.back();
-    //     } else if (this.gesture === "Swiping Left") {
-    //         this.spotifyService.forward();
-    //     }
-    // }
 }
